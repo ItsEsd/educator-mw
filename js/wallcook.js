b79f489f220efdef82ac6076928744b833534815
ItsEsd <81977626+ItsEsd@users.noreply.github.com>
2022-09-09 02:12:43 +0530

    Update wallcook.js

--- js/wallcook.js
+++ js/wallcook.js
@@ -31,6 +31,6 @@ function deleteAllCookies() {
        var cookie = cookies[i];
          var eqPos = cookie.indexOf("=");
        var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
-       document.cookie = name + "=true;"+"expires=Thu, 01 Jan 1970 00:00:00 GMT;path=/";
+       document.cookie = name + "=true;"+"expires=Thu, 01 Jan 1970 00:00:00 GMT;path=/;domain=mastrowall.com";
              }
      }     
