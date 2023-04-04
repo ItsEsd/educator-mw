29a81937957adb2198c173e01115e4ef0cc8bdf6
S Das <d.soubhik@hotmail.com>
2023-04-04 11:39:57 +0530

    Update edu-two.js

--- js/edu-two.js
+++ js/edu-two.js
@@ -74,7 +74,7 @@ function onstartswitch(){
          if (emailch == json.records[i].Email) {
          if(pascd !=json.records[i].Passcode){
     $('#switchnoti').slideDown();       
-    document.getElementById('switchnoti').innerHTML = "Switch to Class: "+json.records[i].Class+" <svg xmlns='http://www.w3.org/2000/svg fill='currentColor' width='20px' height='20px' style='background-color:white;margin-left:10px;padding:2px;' class='bi bi-arrow-left-right' viewBox='0 0 16 16'>"+
+    document.getElementById('switchnoti').innerHTML = "Switch to Class: "+json.records[i].Class+" <svg xmlns='http://www.w3.org/2000/svg fill='currentColor' width='20px' height='20px' style='background-color:white;margin-left:10px;padding:2px;margin-top:-4px;' class='bi bi-arrow-left-right' viewBox='0 0 16 16'>"+
     "<path fill-rule='evenodd' d='M1 11.5a.5.5 0 0 0 .5.5h11.793l-3.147 3.146a.5.5 0 0 0 .708.708l4-4a.5.5 0 0 0 0-.708l-4-4a.5.5 0 0 0-.708.708L13.293 11H1.5a.5.5 0 0 0-.5.5zm14-7a.5.5 0 0 1-.5.5H2.707l3.147 3.146a.5.5 0 1 1-.708.708l-4-4a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 4H14.5a.5.5 0 0 1 .5.5z'/></svg>";
     setTimeout(function(){
         $('#switchnoti').slideUp();},6000);
