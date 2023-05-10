1f06a71a6d2d89d7e9f4414cbd6979ed377ec18f
S Das <d.soubhik@hotmail.com>
2023-05-10 12:33:36 +0530

    Update edu-one.js

--- js/edu-one.js
+++ js/edu-one.js
@@ -191,6 +191,13 @@ if(res[0].TOD!=""){
      w= w+3;
   }
 }
+else{
+  document.getElementById("LiveTOD").style.display = "block";
+  document.getElementById("LiveTOD").innerHTML ='<div class="nocontenttod"><svg xmlns="http://www.w3.org/2000/svg" style="color:#8a8a8b;" width="60" height="60" fill="currentColor" class="bi bi-exclamation-circle" viewBox="0 0 16 16">'+
+      '<path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>'+
+      '<path d="M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 4.995z"/></svg>'+
+      '<br><h5 style="color:#474749;font-size:16px;">No Content</h5></div>';
+}
 rfshcmnt();
 var preevn = res[0].CalendarTODO;
 document.getElementById('allsvevnt').value =preevn;
@@ -265,7 +272,7 @@ function ckeckactvst(dy,mn,yr){
   else if(tyr>yr){
     deleteAllCookies(); $('#notifypay').fadeIn();
   }
-}
+} 
 function live_tod() {
 document.getElementById("updateTOD").style.pointerEvents ="auto";
 document.getElementById("LiveTOD").style.pointerEvents ="auto";
@@ -301,7 +308,7 @@ function ldlivetd(e){
         document.getElementById("loaderTOD").style.display = "none";
       }
     }
-    else if(reslt[0].TOD == ""){
+    else if(reslt[0].TOD == "" ||reslt[0].TOD == null){
       document.getElementById("updateTOD").style.display = "block";
       document.getElementById("LiveTOD").style.display = "block";
       document.getElementById("LiveTOD").innerHTML ='<div class="nocontenttod"><svg xmlns="http://www.w3.org/2000/svg" style="color:#8a8a8b;" width="60" height="60" fill="currentColor" class="bi bi-exclamation-circle" viewBox="0 0 16 16">'+
