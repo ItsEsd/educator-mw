25c52d49737037597881ac8b83be6db55a74f0e5
soubhikdas <d.soubhik@hotmail.com>
2025-10-12 02:03:04 +0530

    up up date .. update ⬆️

--- js/str-tods.js
+++ js/str-tods.js
@@ -555,7 +555,6 @@ function rdsvexm(e) {
   var res = e.records;
   if (res != "ID not found!") {
     if (res[0].AllExam != 0) {
-      $("#prevexperform").empty();
       var allsvexm = res[0].AllExam;
       var singlessvexm = allsvexm.split("{ex},");
       var lenstr = singlessvexm.length;
@@ -597,6 +596,28 @@ function rdsvexm(e) {
 
         srno++;
       }
+    } else if (res[0].AllExam === "") {
+      $("#prevexperform").html(`<div class="nosvdexm">
+                <p>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    style="color: #8a8a8b"
+                    width="60"
+                    height="60"
+                    fill="currentColor"
+                    class="bi bi-info-circle"
+                    viewBox="0 0 16 16"
+                  >
+                    <path
+                      d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"
+                    />
+                    <path
+                      d="M8.93 6.588l-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"
+                    />
+                  </svg>
+                </p>
+                <p>No saved exams for Students.</p>
+              </div>`);
     }
   }
 }
